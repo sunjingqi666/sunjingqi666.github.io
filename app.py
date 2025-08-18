6705a9406156a37acae61d29ab587cdf9d6adf2d
孙敬棋 <3785787772@qq.com>
2025-08-18 14:14:18 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+﻿from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # 解决跨域问题
+
+# 测试路由
+@app.route('/')
+def home():
+    return {"message": "后端部署成功！", "status": "ok"}
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)  # 必须加 host 和 port！
